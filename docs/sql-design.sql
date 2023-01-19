e063189e8881448a940f5484d007acf134798818
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-01-19 14:04:13 +0900

    로그인처리

---
+++ docs/sql-design.sql
@@ -0,0 +1,9 @@
+-- user
+desc user;
+select * from user;
+
+-- join
+insert into user values (null, '안대혁', 'kickscar@gmail.com', password('1234'), 'male', now());
+
+-- login
+select * from user where email = 'kickscar@gmail.com' and password = password('123');
--- mysite02/src/main/java/com/douzone/mysite/dao/UserDao.java
+++ mysite02/src/main/java/com/douzone/mysite/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.douzone.mysite.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.douzone.mysite.vo.UserVo;
@@ -41,7 +42,54 @@ public class UserDao {
 			}
 		}		
 	}
-	
+
+	public UserVo findByEmailAndPassword(UserVo vo) {
+		UserVo result = null;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql = "select no, name from user where email = ? and password = password(?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getEmail());
+			pstmt.setString(2, vo.getPassword());
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = new UserVo();
+				
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				result.setNo(no);
+				result.setName(name);
+			}
+		} catch (SQLException e) {
+			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+		
+		return result;
+	}	
+
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 
@@ -55,5 +103,5 @@ public class UserDao {
 		} 
 		
 		return conn;
-	}	
+	}
 }
