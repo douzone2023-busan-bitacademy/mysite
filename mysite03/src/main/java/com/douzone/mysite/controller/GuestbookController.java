fd86f75744b3ffef2b89d02477a7adc24ac115db
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-02-01 14:58:11 +0900

    mybatis #final

--- mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -29,7 +29,9 @@ public class GuestbookController {
 	
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	public String add(GuestbookVo vo) {
+		System.out.println(vo);
 		guestbookService.addMessage(vo);
+		System.out.println(vo);
 		return "redirect:/guestbook";
 	}
 	
@@ -43,10 +45,5 @@ public class GuestbookController {
 	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
 		guestbookService.deleteMessage(no, password);
 		return "redirect:/guestbook";
-	}
-	
-	@RequestMapping("/spa")
-	public String spaLanding() {
-		return "guestbook/index-spa";
 	}
 }
