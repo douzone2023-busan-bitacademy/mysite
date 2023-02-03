d15a6956cc1cdc63d8331472584fe0d929b1958e
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2023-02-03 17:11:48 +0900

    Add files via upload

--- mysite03/src/main/java/com/douzone/mysite/service/BoardService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/BoardService.java
@@ -1,4 +1,4 @@
-package com.douzone.mysite.service;
+package me.kickscar.mysite.service;
 
 import java.util.HashMap;
 import java.util.List;
@@ -7,53 +7,94 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.douzone.mysite.repository.BoardRepository;
-import com.douzone.mysite.vo.BoardVo;
+import me.kickscar.mysite.repository.BoardRepository;
+import me.kickscar.mysite.vo.BoardVo;
 
 @Service
 public class BoardService {
-	private static final int LIST_SIZE = 5; // 리스팅되는 게시물의 수
-	private static final int PAGE_SIZE = 5; // 페이지 리스트의 페이지 수
+	private static final int LIST_SIZE = 5; //리스팅되는 게시물의 수
+	private static final int PAGE_SIZE = 5; //페이지 리스트의 페이지 수
 	
 	@Autowired
 	private BoardRepository boardRepository;
 	
-	public void addContents(BoardVo vo) {
-	}
-
-	public BoardVo getContents(Long no) {
-		return null;
-	}
-	
-	public BoardVo getContents(Long no, Long userNo) {
-		return null;
+	public boolean addContents( BoardVo boardVo ) {
+		if( boardVo.getGroupNo() != null ) {
+			increaseGroupOrderNo( boardVo );
+		}
+		return boardRepository.insert( boardVo ) == 1;
 	}
 	
-	public void updateContents(BoardVo vo) {
-	}
-	
-	public void deleteContents(Long no, Long userNo) {
-	}
-	
-	public Map<String, Object> getContentsList(int page, String keyword) {
-		int toTalCount = boardRepository.getTotalCount(keyword);
-
-		// 1. view에서 게시판 리스트를 렌더링 하기 위한 데이터 값 계산
-		int beginPage = 0;
-		int prevPage = 0;
-		int nextPage = 0;
-		int endPage = 0;
-
-		//2. 리스트 가져오기
-		List<BoardVo> list = boardRepository.findAllByPageAndKeyword(page, keyword, LIST_SIZE);
+	public BoardVo getContents( Long no ) {
+		BoardVo boardVo = boardRepository.findByNo( no );
 		
-		//3. 리스트 정보를 맴에 저장
-		Map<String, Object> map = new HashMap<>();
-		map.put("list", list);
-		map.put("beginPage", beginPage);
-		// ...
-		// ...
+		if( boardVo != null ) {
+			boardRepository.updateHit( no );
+		}
 		
+		return boardVo;
+	}
+
+	public BoardVo getContents( Long no, Long userNo ) {
+		BoardVo boardVo = boardRepository.findByNoAndUserNo( no, userNo );
+		return boardVo;
+	}
+	
+	public boolean modifyContents( BoardVo boardVo ) {
+		int count = boardRepository.update( boardVo );
+		return count == 1;
+	}
+	
+	public boolean deleteContents( Long boardNo, Long userNo ) {
+		int count = boardRepository.delete( boardNo, userNo );
+		return count == 1;
+	}
+	
+	public Map<String, Object> getContentsList( int currentPage, String keyword ){
+		
+		//1. 페이징을 위한 기본 데이터 계산
+		int totalCount = boardRepository.getTotalCount( keyword ); 
+		int pageCount = (int)Math.ceil( (double)totalCount / LIST_SIZE );
+		int blockCount = (int)Math.ceil( (double)pageCount / PAGE_SIZE );
+		int currentBlock = (int)Math.ceil( (double)currentPage / PAGE_SIZE );
+		
+		//2. 파라미터 page 값  검증
+		if( currentPage > pageCount ) {
+			currentPage = pageCount;
+			currentBlock = (int)Math.ceil( (double)currentPage / PAGE_SIZE );
+		}		
+		
+		if( currentPage < 1 ) {
+			currentPage = 1;
+			currentBlock = 1;
+		}
+		
+		//3. view에서 페이지 리스트를 렌더링 하기위한 데이터 값 계산
+		int beginPage = currentBlock == 0 ? 1 : (currentBlock - 1) * PAGE_SIZE + 1;
+		int prevPage = ( currentBlock > 1 ) ? ( currentBlock - 1 ) * PAGE_SIZE : 0;
+		int nextPage = ( currentBlock < blockCount ) ? currentBlock * PAGE_SIZE + 1 : 0;
+		int endPage = ( nextPage > 0 ) ? ( beginPage - 1 ) + LIST_SIZE : pageCount;
+		
+		//4. 리스트 가져오기
+		List<BoardVo> list = boardRepository.findAllByPageAndKeword( keyword, currentPage, LIST_SIZE );
+		
+		//5. 리스트 정보를 맵에 저장
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put( "list", list );
+		map.put( "totalCount", totalCount );
+		map.put( "listSize", LIST_SIZE );
+		map.put( "currentPage", currentPage );
+		map.put( "beginPage", beginPage );
+		map.put( "endPage", endPage );
+		map.put( "prevPage", prevPage );
+		map.put( "nextPage", nextPage );
+		map.put( "keyword", keyword );
+
 		return map;
+	}
+	
+	public boolean increaseGroupOrderNo( BoardVo boardVo ) {
+		return boardRepository.updateOrderNo( boardVo.getGroupNo(), boardVo.getOrderNo() ) > 0;
 	}
 }
