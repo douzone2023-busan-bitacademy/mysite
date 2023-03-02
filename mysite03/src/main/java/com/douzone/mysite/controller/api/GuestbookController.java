c89e4635c122028c3bfa72ccf6f10c72f6a481c0
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2023-03-02 18:53:00 +0900

    Update GuestbookController.java

--- mysite03/src/main/java/com/douzone/mysite/controller/api/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/api/GuestbookController.java
@@ -13,6 +13,25 @@ public class GuestbookController {
 	@Autowired
 	private GuestbookService guestbookService;
 	
-	
+	@GetMapping("")
+	public JSONResult list(@RequestParam(value="sno", required=true, defaultValue="0") Long startNo) {
+		List<GuestbookVo> list = guestbookService.getMessageList(startNo);
+		return JsonResult.success(list);
+	}
+
+	@PostMapping("")
+	public JSONResult add(@RequestBody GuestbookVo vo) {
+		guestbookService.addMessage(vo);
+		vo.setPassword("");
+		return JsonResult.success(vo);
+	}
+
+	@DeleteMapping("/{no}")
+	public JSONResult delete(
+			@PathVariable("no") Long no,
+			@RequestParam(value="password", required=true, defaultValue="") String password) {
+		boolean result = guestbookService.deleteMessage(no, password);
+		return JsonResult.success(result ? no : -1);
+	}	
 	
 }
